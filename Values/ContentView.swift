96398c12b0f11828291ce6199afe8543e7134692
Igor <pajkert.igor@gmail.com>
2025-05-30 18:22:34 +0200

    Initial Commit

---
+++ Values/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Values
+//
+//  Created by Igor Pajkert on 30/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Values/ValuesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValuesApp.swift
+//  Values
+//
+//  Created by Igor Pajkert on 30/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ValuesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
